60337fc57deceb7e4fcbe09d70370cf10824c8a1
GustavoRuedaEnriquez <gusgus2208@gmail.com>
2019-04-29 07:14:48 -0500

    M4 almost functional

--- M4.py
+++ M4.py
@@ -6,17 +6,34 @@ def parseToDFA(nfaInitialState,nfaFinalStates,nfaTransitions):
     alphabetSize   = len(nfaTransitions[0])
     newestState    = 0
 
-    dfaTransitions.append(None * len(nfaTransitions[0]))
-    dfaNewStates[newestState] = set(nfaInitialState)
+    dfaTransitions.append([None] * len(nfaTransitions[0]))
+    dfaNewStates[newestState] = set([nfaInitialState])
 
     currentStates = 0
     currentSize   = len(dfaTransitions)
 
-    while(dfaStates < currentSize):
+    while(currentStates < currentSize):
         nfaSet = dfaNewStates[currentStates]
         for i in range(alphabetSize):
             tempState = set()
             for j in nfaSet:
                 tempTransition = nfaTransitions[j][i]
+                if type(tempTransition) is int :
+                    tempTransition = [tempTransition]
+                elif tempTransition == None:
+                    tempTransition = []
+                tempState = tempState.union(set(tempTransition))
+            if tempState not in dfaNewStates.values():
+                newestState += 1
+                dfaNewStates[newestState] = tempState
+                if (not nfaFinalStates.isdisjoint(tempState)):
+                    dfaFinalStates.add(newestState)
+                dfaTransitions.append([None]*alphabetSize)
+            dfaTransitions[currentStates][i] = list(dfaNewStates.keys())[list(dfaNewStates.values()).index(tempState)]
+        currentStates += 1
+        currentSize = len(dfaTransitions)
+    return nfaInitialState,dfaFinalStates,dfaTransitions
 
-    
+nfa = [[[0,2],1],[[1,2],2],[[0,2],None]]
+finalStates = set([0])
+print(parseToDFA(0,finalStates,nfa))
