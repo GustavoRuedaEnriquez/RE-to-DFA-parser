f4b4942653631fcd11e64f9707f1ed3551bbae37
Carlo Muñoz <carlo@sommusa.com.mx>
2019-05-19 17:53:18 -0500

    M0 implemented

--- M0.py
+++ M0.py
@@ -1,5 +1,9 @@
-def replaceWildcard(str,alphabet):
-    replacement = getReplacement(alphabet)
+def M0(str):
+    replacement  = '(a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z,á,é,í,ó,ú,'
+    replacement += 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,Á,É,Í,Ó,Ú,'
+    replacement += '1,2,3,4,5,6,7,8,9,0, ,\n)'
+
+    # replacement = getReplacement(alphabet)
     new_str = str.replace('&',replacement)
     return new_str
 
@@ -15,7 +19,9 @@ def getReplacement(alphabet):
     result_str += ")"
     return result_str
 
-alphabet = ['a','b','c']
-str = "ab&+(a,b,&)"
-print("Input: " + str)
-print("Output: " + replaceWildcard(str,alphabet))
+
+if __name__ == '__main__':
+    alphabet = ['a','b','c']
+    str = "ab&+(a,b,&)"
+    print("Input: " + str)
+    print("Output: " + M0(str))
